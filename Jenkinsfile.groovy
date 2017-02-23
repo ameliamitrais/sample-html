aaca68469f91ab2937a8075e2b5a8f7b53ca3317
Amelia Rahman <amelia.rahman@mitrais.com>
2017-02-23 17:46:54 +0800

    Update Jenkinsfile.groovy
    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurperClassic
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
-	def applicationName = 'amelia-app' //change me
+	def applicationName = 'amelia-app'
 	def deploymentGroupName = 'amelia-app-group' // change me
 	def s3BucketName = 'deployment-cdc'
 	
@@ -13,16 +13,7 @@ node {
 
 	stage('Checkout') {
 		checkout scm	
-	}
-	
-	
-	stage("Sonar Analyze") {
-		def scannerHome = tool 'default';
-	    withSonarQubeEnv('default') {
-	      sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${deploymentGroupName} -Dsonar.sources=app"
-	    }
-	}
-	
+	}	
 
 	stage('Deploy') {
 		withCredentials([[$class          : 'UsernamePasswordMultiBinding',
